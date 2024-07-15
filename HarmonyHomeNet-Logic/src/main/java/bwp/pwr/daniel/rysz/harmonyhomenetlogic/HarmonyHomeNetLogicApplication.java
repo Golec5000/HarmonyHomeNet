172f2e0bd0c63bfdeb6e5a5120e365dca2319108
Wombat5000 <danielryszkowski@gmail.com>
2024-07-15 17:04:37 +0200

    spring-boot init

---
+++ HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/HarmonyHomeNetLogicApplication.java
@@ -0,0 +1,13 @@
+package bwp.pwr.daniel.rysz.harmonyhomenetlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyHomeNetLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyHomeNetLogicApplication.class, args);
+	}
+
+}
---
+++ HarmonyHomeNet-Logic/src/test/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/HarmonyHomeNetLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package bwp.pwr.daniel.rysz.harmonyhomenetlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyHomeNetLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
