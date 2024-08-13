37a0349cc9dc0677d412718351a3ea9d6856b1ac
Wombat5000-PC <danielryszkowski@gmail.com>
2024-08-14 00:19:37 +0200

    proper pesel number check

--- HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/mainLogicEntitys/user/entity/User.java
+++ HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/mainLogicEntitys/user/entity/User.java
@@ -4,11 +4,12 @@ package bwp.pwr.daniel.rysz.harmonyhomenetlogic.mainLogicEntitys.user.entity;
 import bwp.pwr.daniel.rysz.harmonyhomenetlogic.utils.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -52,14 +53,53 @@ public class User {
     @PrePersist
     @PreUpdate
     public void checkPESELNumber(){
-        if (PESELNumber == null || PESELNumber.isEmpty() || PESELNumber.isBlank())
+        if (PESELNumber == null || PESELNumber.isBlank())
             throw new IllegalArgumentException("PESEL number cannot be null or empty");
 
-        if (!PESELNumber.chars().allMatch(Character::isDigit))
-            throw new IllegalArgumentException("PESEL number must contain only digits");
-
-        if (PESELNumber.length() != 11)
+        if (!PESELNumber.matches("\\d{11}"))
             throw new IllegalArgumentException("PESEL number must contain 11 digits");
 
+        if (!isChecksumValid(PESELNumber))
+            throw new IllegalArgumentException("Invalid PESEL checksum");
+
+        if (!isDateValid(PESELNumber))
+            throw new IllegalArgumentException("Invalid PESEL date");
+
+        if (isFutureDate(PESELNumber))
+            throw new IllegalArgumentException("PESEL date cannot be in the future");
+    }
+
+    private boolean isChecksumValid(String pesel) {
+        int[] weights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += Character.getNumericValue(pesel.charAt(i)) * weights[i];
+        }
+        int checksum = (10 - (sum % 10)) % 10;
+        return checksum == Character.getNumericValue(pesel.charAt(10));
+    }
+
+    private boolean isDateValid(String pesel) {
+        List<Integer> tmp = dates(pesel);
+
+        try {
+            LocalDate date = LocalDate.of(tmp.get(0), tmp.get(1), tmp.get(2));
+            return date.getDayOfMonth() == tmp.get(2);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isFutureDate(String pesel) {
+        List<Integer> tmp = dates(pesel);
+        LocalDate date = LocalDate.of(tmp.get(0), tmp.get(1), tmp.get(2));
+        return date.isAfter(LocalDate.now());
+    }
+
+    private List<Integer> dates(String pesel) {
+        int year = Integer.parseInt(pesel.substring(0, 2));
+        int month = Integer.parseInt(pesel.substring(2, 4));
+        int day = Integer.parseInt(pesel.substring(4, 6));
+        return List.of(year, month, day);
     }
 }
--- HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/utils/DatabaseInit.java
+++ HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/utils/DatabaseInit.java
@@ -16,6 +16,8 @@ import bwp.pwr.daniel.rysz.harmonyhomenetlogic.mainLogicEntitys.user.entity.Resi
 import bwp.pwr.daniel.rysz.harmonyhomenetlogic.mainLogicEntitys.user.repository.ResidentRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -25,6 +27,7 @@ import java.util.Random;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DatabaseInit {
 
     private final BuildingService buildingService;
@@ -41,85 +44,94 @@ public class DatabaseInit {
     @PostConstruct
     public void init() {
 
-        for (int i = 0; i < 20; i++) {
-            Resident resident = Resident.builder()
-                    .login("user" + i)
-                    .PESELNumber(PeselGenerator())
-                    .email("user" + i + "@gmail.com")
-                    .build();
-            residentRepository.save(resident);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            Building buildingToSave = Building.builder()
-                    .buildingName("Blok " + (i + 1))
-                    .street("ul. Długa " + i)
-                    .city("Kraków")
-                    .region("Małopolskie")
-                    .build();
-
-            buildingService.save(buildingToSave);
-
-            List<Basement> basements = new ArrayList<>();
-            for (int j = 0; j < 5; j++) {
-                Basement basement = Basement.builder()
-                        .area(BigDecimal.valueOf((new Random().nextInt(15) + 6) * (j + 1)))
-                        .basementNumber(j + 1)
-                        .building(buildingToSave)
+        try {
+            for (int i = 0; i < 10; i++) {
+                Resident resident = Resident.builder()
+                        .login("user" + i)
+                        .PESELNumber(PeselGenerator(i))
+                        .email("user" + i + "@gmail.com")
                         .build();
-
-                basementService.save(basement);
-                basements.add(basement);
+                residentRepository.save(resident);
             }
 
-            List<ParkingSpace> parkingSpaces = new ArrayList<>();
-            for (int j = 0; j < 5; j++) {
-                ParkingSpace parkingSpace = ParkingSpace.builder()
-                        .number(j + 1)
-                        .building(buildingToSave)
+            for (int i = 0; i < 10; i++) {
+                Building buildingToSave = Building.builder()
+                        .buildingName("Blok " + (i + 1))
+                        .street("ul. Długa " + i)
+                        .city("Kraków")
+                        .region("Małopolskie")
                         .build();
 
-                parkingSpaceService.save(parkingSpace);
-                parkingSpaces.add(parkingSpace);
+                buildingService.save(buildingToSave);
+
+                List<Basement> basements = new ArrayList<>();
+                for (int j = 0; j < 5; j++) {
+                    Basement basement = Basement.builder()
+                            .area(BigDecimal.valueOf((new Random().nextInt(15) + 6) * (j + 1)))
+                            .basementNumber(j + 1)
+                            .building(buildingToSave)
+                            .build();
+
+                    basementService.save(basement);
+                    basements.add(basement);
+                }
+
+                List<ParkingSpace> parkingSpaces = new ArrayList<>();
+                for (int j = 0; j < 5; j++) {
+                    ParkingSpace parkingSpace = ParkingSpace.builder()
+                            .number(j + 1)
+                            .building(buildingToSave)
+                            .build();
+
+                    parkingSpaceService.save(parkingSpace);
+                    parkingSpaces.add(parkingSpace);
+                }
+
+                // New code for adding apartments
+                List<Apartment> apartments = new ArrayList<>();
+                for (int k = 0; k < 10; k++) {
+                    Apartment apartment = Apartment.builder()
+                            .apartmentNumber(k + 1)
+                            .area(BigDecimal.valueOf(50.0 + (k + 1) * 5.0 * new Random().nextDouble())) // Example area calculation
+                            .building(buildingToSave)
+                            .build();
+
+                    apartmentService.save(apartment);
+                    apartments.add(apartment);
+                }
+
+                buildingToSave.setParkingSpaces(parkingSpaces);
+                buildingToSave.setBasements(basements);
+                // Assuming Building entity has a field to reference apartments
+                buildingToSave.setApartments(apartments);
+                buildingService.save(buildingToSave); // update building with basements, parking spaces, and apartments
             }
 
-            // New code for adding apartments
-            List<Apartment> apartments = new ArrayList<>();
-            for (int k = 0; k < 10; k++) {
-                Apartment apartment = Apartment.builder()
-                        .apartmentNumber(k + 1)
-                        .area(BigDecimal.valueOf(50.0 + (k + 1) * 5.0 * new Random().nextDouble())) // Example area calculation
-                        .building(buildingToSave)
+            for (int i = 0; i < 2; i++) {
+                Forum forum = Forum.builder()
+                        .forumDescription("Test desc." + (i + 1))
+                        .forumName("Forum" + (i + 1))
                         .build();
 
-                apartmentService.save(apartment);
-                apartments.add(apartment);
+                forumRepository.save(forum);
             }
-
-            buildingToSave.setParkingSpaces(parkingSpaces);
-            buildingToSave.setBasements(basements);
-            // Assuming Building entity has a field to reference apartments
-            buildingToSave.setApartments(apartments);
-            buildingService.save(buildingToSave); // update building with basements, parking spaces, and apartments
+        } catch (Exception e) {
+            log.error("Error while initializing database: {}", e.getMessage());
         }
-
-        for (int i = 0; i < 2; i++) {
-            Forum forum = Forum.builder()
-                    .forumDescription("Test desc." + (i + 1))
-                    .forumName("Forum" + (i + 1))
-                    .build();
-
-            forumRepository.save(forum);
-        }
-
-
-
     }
 
-    private String PeselGenerator() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 11; i++) sb.append(new Random().nextInt(10));
-        return sb.toString();
+    private String PeselGenerator(int index) {
+        return List.of(
+                "00212771198",
+                "01271849549",
+                "78091078723",
+                "72111861654",
+                "69072751847",
+                "93102344147",
+                "67061148845",
+                "82061159354",
+                "49070653334",
+                "60102554238"
+        ).get(index);
     }
-
 }
