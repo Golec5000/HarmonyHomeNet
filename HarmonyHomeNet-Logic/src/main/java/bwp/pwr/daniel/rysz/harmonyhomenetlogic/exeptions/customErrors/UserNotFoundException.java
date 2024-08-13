83175fb28f2a53f13105b46dc72d283857efbb46
Wombat5000-PC <danielryszkowski@gmail.com>
2024-08-13 23:36:31 +0200

    exeptions update

--- HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/exeptions/customErrors/UserNotFoundException.java
+++ HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/exeptions/customErrors/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package bwp.pwr.daniel.rysz.harmonyhomenetlogic.exeptions.customErrors;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super("User is not found: " + message);
     }
