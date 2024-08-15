bf641d0bd92c723ec505f0b89124702a935fe9cb
Wombat5000-PC <danielryszkowski@gmail.com>
2024-08-15 12:02:03 +0200

    remove of unnesery repositorys

--- HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/mainLogicEntitys/user/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package bwp.pwr.daniel.rysz.harmonyhomenetlogic.mainLogicEntitys.user.repository;
-
-import bwp.pwr.daniel.rysz.harmonyhomenetlogic.mainLogicEntitys.user.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
-}
