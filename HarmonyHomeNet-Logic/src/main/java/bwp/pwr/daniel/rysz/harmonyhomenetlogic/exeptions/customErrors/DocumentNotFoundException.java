57a8f2182ee058c7d99a9db407ee66824ef992aa
wombat-laptop <danielryszkowski@gmail.com>
2024-10-23 00:10:53 +0200

    removing unnecessary files from the diploma branch

--- HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/exeptions/customErrors/DocumentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bwp.pwr.daniel.rysz.harmonyhomenetlogic.exeptions.customErrors;
-
-public class DocumentNotFoundException extends RuntimeException {
-    public DocumentNotFoundException(String message) {
-        super("Document is not found" + message);
-    }
-}
--- HarmonyHomeNet-Logic/src/main/java/bwp/pwr/daniel/rysz/harmonyhomenetlogic/exeptions/customErrors/TopicNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bwp.pwr.daniel.rysz.harmonyhomenetlogic.exeptions.customErrors;
-
-public class TopicNotFoundException extends RuntimeException {
-    public TopicNotFoundException(String message) {
-        super("Topic not found: " +message);
-    }
-}
