6e3a0fe880ac62aaa2d3b79941d24409ff269c76
wombat-pc-ubuntu <danielryszkowski@gmail.com>
2024-11-29 18:37:27 +0100

    basic compose ready for prototipe

--- backend/src/main/java/bwp/hhn/backend/harmonyhomenetlogic/utils/initPackage/InitClass.java
+++ backend/src/main/java/bwp/hhn/backend/harmonyhomenetlogic/utils/initPackage/InitClass.java
@@ -1,46 +1,50 @@
 package bwp.hhn.backend.harmonyhomenetlogic.utils.initPackage;
 
-import bwp.hhn.backend.harmonyhomenetlogic.entity.mainTables.Apartment;
 import bwp.hhn.backend.harmonyhomenetlogic.entity.mainTables.User;
-import bwp.hhn.backend.harmonyhomenetlogic.entity.sideTables.PossessionHistory;
-import bwp.hhn.backend.harmonyhomenetlogic.repository.mainTables.ApartmentsRepository;
 import bwp.hhn.backend.harmonyhomenetlogic.repository.mainTables.UserRepository;
-import bwp.hhn.backend.harmonyhomenetlogic.repository.sideTables.PossessionHistoryRepository;
 import bwp.hhn.backend.harmonyhomenetlogic.utils.enums.Role;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.Instant;
 import java.util.Optional;
-import java.util.Random;
 
 @Component
 @RequiredArgsConstructor
 public class InitClass {
 
     private final UserRepository userRepository;
-    private final ApartmentsRepository apartmentsRepository;
-    private final PossessionHistoryRepository possessionHistoryRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Value("${SUPER_ADMIN_FIRST_NAME}")
+    private String superAdminFirstName;
+
+    @Value("${SUPER_ADMIN_LAST_NAME}")
+    private String superAdminLastName;
+
+    @Value("${SUPER_ADMIN_EMAIL}")
+    private String superAdminEmail;
+
+    @Value("${SUPER_ADMIN_PASSWORD}")
+    private String superAdminPassword;
+
+    @Value("${SUPER_ADMIN_PHONE}")
+    private String superAdminPhone;
 
     @PostConstruct
     public void init() {
         System.out.println("Init class");
 
-        User john = createUserIfNotExists("John", "Doe", "john.doe@example.com", bCryptPasswordEncoder.encode("password123"), "123456789", Role.ROLE_ADMIN);
-        User jane = createUserIfNotExists("Jane", "Smith", "jane.smith@example.com", bCryptPasswordEncoder.encode("password123"), "987654321", Role.ROLE_OWNER);
-        User alice = createUserIfNotExists("Alice", "Johnson", "alice.johnson@example.com", bCryptPasswordEncoder.encode("password123"), "555555555", Role.ROLE_OWNER);
-        User SA = createUserIfNotExists("Daniel", "Ryszkowski", "daniel.hhn.SA@gmail.com", bCryptPasswordEncoder.encode("password123"), "111111111", Role.ROLE_SUPER_ADMIN);
-
-        Apartment apartment1 = createApartment("Balonowa 5/10", "Kraków", "30-000", "SIG001");
-        Apartment apartment2 = createApartment("Zakopiańska 12/100", "Kraków", "00-000", "SIG002");
-        Apartment apartment3 = createApartment("Kwiatowa 1/1", "Kraków", "80-000", "SIG003");
-
-        createPossessionHistory(jane, apartment1);
+        User SA = createUserIfNotExists(
+                superAdminFirstName,
+                superAdminLastName,
+                superAdminEmail,
+                bCryptPasswordEncoder.encode(superAdminPassword),
+                superAdminPhone,
+                Role.ROLE_SUPER_ADMIN
+        );
     }
 
     private User createUserIfNotExists(String firstName, String lastName, String email, String password, String phoneNumber, Role role) {
@@ -57,33 +61,5 @@ public class InitClass {
             return userRepository.save(user);
         }
         return existingUser.get();
-    }
-
-    private Apartment createApartment(String address, String city, String zipCode, String signature) {
-        Optional<Apartment> existingApartments = apartmentsRepository.findByApartmentSignature(signature);
-        if (existingApartments.isEmpty()) {
-            Apartment apartment = Apartment.builder()
-                    .address(address)
-                    .city(city)
-                    .zipCode(zipCode)
-                    .apartmentArea(BigDecimal.valueOf(new Random().nextDouble() * 100).setScale(2, RoundingMode.HALF_UP))
-                    .apartmentPercentValue(BigDecimal.valueOf(50.00))
-                    .apartmentSignature(signature)
-                    .build();
-            return apartmentsRepository.save(apartment);
-        }
-        return existingApartments.get();
-    }
-
-    private void createPossessionHistory(User user, Apartment apartment) {
-        if (possessionHistoryRepository.existsByUserAndApartment(user, apartment)) {
-            return;
-        }
-        PossessionHistory possessionHistory = PossessionHistory.builder()
-                .user(user)
-                .apartment(apartment)
-                .startDate(Instant.now())
-                .build();
-        possessionHistoryRepository.save(possessionHistory);
     }
 }
